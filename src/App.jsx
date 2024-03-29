91ef0ba28ec34499c860085ba2e080e95c0df7d9
dennis-tech <hedenniskibe@gmail.com>
2024-03-29 06:10:26 +0300

    Added HowItWorks component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Highlights from './components/Highlights'
 import Model from "./components/Model"
 import * as Sentry from '@sentry/react'
 import Features from './components/Features'
+import HowItWorks from './components/HowItWorks'
 
 const  App = () => {
 
@@ -14,6 +15,7 @@ const  App = () => {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
     </main>
   )
 }
